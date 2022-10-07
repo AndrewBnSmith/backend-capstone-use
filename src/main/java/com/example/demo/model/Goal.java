6cc4ee97477fe0bf6a648bb872c3abdc9f40b21d
AndrewBnSmith <absmith1058@outlook.com>
2022-10-07 16:58:40 -0400

    added goal fields

--- src/main/java/com/example/demo/model/Goal.java
+++ src/main/java/com/example/demo/model/Goal.java
@@ -18,7 +18,46 @@ public class Goal {
 	private long id;
 	@Column(name = "name")
 	private String name;
+	@Column(name = "goalTotal")
+	private double goalTotal;
+	@Column(name = "contribute")
+	private double contribute;
+	@Column(name = "fromDate")
+	private String fromDate;
+	@Column(name = "toDate")
+	private String toDate;
 
+	public double getGoalTotal() {
+		return goalTotal;
+	}
+
+	public void setGoalTotal(double goalTotal) {
+		this.goalTotal = goalTotal;
+	}
+
+	public double getContribute() {
+		return contribute;
+	}
+
+	public void setContribute(double contribute) {
+		this.contribute = contribute;
+	}
+
+	public String getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public String getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
 
 	public long getId() {
 		return id;
